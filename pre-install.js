a2a7c400edb4e67776901040af292e7017bf8d03
Luke Smetham <luke@smetham.dev>
2022-11-05 21:52:16 +0100

    release: build v0.2.1

--- pre-install.js
+++ pre-install.js
@@ -28,8 +28,8 @@ if (fs.existsSync(cargoDir)) {
     
 const features = process.env.npm_config_features ? `--features ${process.env.npm_config_features.replace(",", " ")}` : ""; 
 
-console.log(`Installing and compiling tokenparser 0.1.0 ${features} ...`);
-exec(`cargo install tokenparser --vers 0.1.0 ${features}`, (error, stdout, stderr) => {
+console.log(`Installing and compiling tokenparser 0.2.1 ${features} ...`);
+exec(`cargo install tokenparser --vers 0.2.1 ${features}`, (error, stdout, stderr) => {
   console.log(stdout);
   if (error || stderr) {
     console.log(error || stderr);
