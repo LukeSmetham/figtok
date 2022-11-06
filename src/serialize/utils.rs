4181ff1392717f685c731ae7bfeaa4cf6ccffc6f
Luke Smetham <luke@smetham.dev>
2022-11-06 13:40:39 +0100

    Update utils.rs

--- src/serialize/utils.rs
+++ src/serialize/utils.rs
@@ -8,14 +8,14 @@ use regex::{Captures, Regex};
 
 /// Tests if a value is a static value or a reference. If static it's returned as is,
 /// whereas if it's a reference we go and retrieve the token, and update the value.
-pub fn get_token_value(loader: &Loader, og_token: &TokenDefinition) -> String {
+pub fn get_token_value(loader: &Loader, token: &TokenDefinition) -> String {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"\{([x[^\{\}]]*)\}.*?").unwrap();
     }
 
     // Check if the original_value contains handlebar syntax with a reference to another token.
-    let mut value = if RE.is_match(&og_token.value) {
-        RE.replace_all(&og_token.value, |caps: &Captures| { // this will run for each occurrence per string. (i.e. multiple tokens multiplied together)
+    let mut value = if RE.is_match(&token.value) {
+        RE.replace_all(&token.value, |caps: &Captures| { // this will run for each occurrence per string. (i.e. multiple tokens multiplied together)
             // Get the ref string without the surrounding curly brackets and use it to retrieve the referenced token
             let ref_name = &caps[1];
 
@@ -27,7 +27,7 @@ pub fn get_token_value(loader: &Loader, og_token: &TokenDefinition) -> String {
 						.replace(&caps[0], format!("var(--{})", t.name.replace(".", "-")))
 						.to_string();
 
-					if !og_token.value.starts_with("rgb") && og_token.kind == TokenKind::Color {
+					if !token.value.starts_with("rgb") && token.kind == TokenKind::Color {
 						new_value = format!("rgb({})", new_value);
 					}
 
@@ -36,12 +36,12 @@ pub fn get_token_value(loader: &Loader, og_token: &TokenDefinition) -> String {
                 None => {
                     let mut new_value = RE
                         .replace_all(
-                            &og_token.value.to_string(),
+                            &token.value.to_string(),
                             format!("var(--{})", ref_name.replace(".", "-")),
                         )
                         .to_string();
 
-                    if !og_token.value.starts_with("rgb") && og_token.kind == TokenKind::Color {
+                    if !token.value.starts_with("rgb") && token.kind == TokenKind::Color {
                         new_value = format!("rgb({})", new_value);
                     }
                     new_value
@@ -51,7 +51,7 @@ pub fn get_token_value(loader: &Loader, og_token: &TokenDefinition) -> String {
         .to_string()
     } else {
         // If there is no handlebar reference in the string, just return the value as is.
-        og_token.value.clone()
+        token.value.clone()
     };
 
     // TODO: This needs improving.
