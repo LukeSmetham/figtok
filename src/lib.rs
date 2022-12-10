b07844ef74f7494d226be145f4ba744ff5c5cde8
Luke Smetham <luke@smetham.dev>
2022-12-10 11:11:27 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -25,31 +25,37 @@ pub struct Figtok<T: Loader> {
 
 impl <T: Loader + Default> Figtok<T> {
     pub fn create(entry_path: &String, output_path: &String) -> Result<Figtok<T>, Box<dyn Error>> {
-        // Check output directory exists, and destroy it if truthy so we can clear any existing output files.
-        if Path::new(&output_path).is_dir() {
-            fs::remove_dir_all(&output_path)?;
+		let ft = Figtok {
+			entry_path: entry_path.clone(),
+			output_path: output_path.clone(),
+			loader: T::default(),
+			serializer: Box::new(CssSerializer::new()),
+		};
+
+		let _ = ft.prepare();
+
+        Ok(ft)
+    }
+
+	fn prepare(&self) -> Result<(), Box<dyn Error>> {
+		// Check output directory exists, and destroy it if truthy so we can clear any existing output files.
+        if Path::new(&self.output_path).is_dir() {
+            fs::remove_dir_all(&self.output_path)?;
         }
 
         // Now ensure the output_path dir exists.
-        fs::create_dir_all(&output_path)?;
+        fs::create_dir_all(&self.output_path)?;
 
         // Check if the input directory exists
-        if !Path::new(&entry_path).exists() {
+        if !Path::new(&self.entry_path).exists() {
             return Err(Box::new(std::io::Error::new(
                 std::io::ErrorKind::NotFound,
-                format!("No {} directory found, passed as input directory", entry_path),
+                format!("No {} directory found, passed as input directory", self.entry_path),
             )));
-        }
+        };
 
-        Ok(
-            Figtok {
-                entry_path: entry_path.clone(),
-                output_path: output_path.clone(),
-                loader: T::default(),
-                serializer: Box::new(CssSerializer::new()),
-            }
-        )
-    }
+		Ok(())
+	}
 
     pub fn load(&mut self) {
         let _ = &mut self.loader.load(&self.entry_path);
