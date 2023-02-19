f40b58c58538528ff0e7d4b58d6b9cf187eed8aa
Luke Smetham <luke@smetham.dev>
2023-02-19 05:53:00 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use load::load;
 pub mod serialize;
 use serialize::{Serializer, CssSerializer, JsonSerializer};
 
-use std::{error::Error, collections::HashMap};
+use std::{collections::HashMap};
 use std::fs;
 use std::path::Path;
 
@@ -45,32 +45,23 @@ impl Figtok {
 			serializer,
 		};
 
-		let _ = figtok.prepare();
-
-		figtok
-    }
-
-	fn prepare(&mut self) -> Result<(), Box<dyn Error>> {
 		// Check output directory exists, and destroy it if truthy so we can clear any existing output files.
-        if Path::new(&self.output_path).is_dir() {
-            fs::remove_dir_all(&self.output_path)?;
+        if Path::new(&figtok.output_path).is_dir() {
+            fs::remove_dir_all(&figtok.output_path).unwrap();
         }
 
         // Now ensure the output_path dir exists.
-        fs::create_dir_all(&self.output_path)?;
+        fs::create_dir_all(&figtok.output_path).unwrap();
 
         // Check if the input directory exists
-        if !Path::new(&self.entry_path).exists() {
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                format!("No {} directory found, passed as input directory", self.entry_path),
-            )));
+        if !Path::new(&figtok.entry_path).exists() {
+            panic!("No {} directory found, passed as input directory", figtok.entry_path);
         };
 
-		load(self);
+		load(&mut figtok);
 
-		Ok(())
-	}
+		figtok
+    }
 
     pub fn export(&self) {
         self.serializer.serialize(self);
