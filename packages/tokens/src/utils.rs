79197054aa431530fd575160bde15f0b6b1e257d
Luke Smetham <luke@smetham.dev>
2023-04-08 20:38:41 +0200

    added tests and fixed a bug in css_stringify

--- packages/tokens/src/utils.rs
+++ packages/tokens/src/utils.rs
@@ -1,5 +1,23 @@
-use convert_case::{Case, Casing};
+use convert_case::{Boundary, Case, Casing};
 
 pub fn css_stringify(s: &String) -> String {
-	s.replace(".", "-").to_case(Case::Kebab)
+	s.replace(".", "-").with_boundaries(&[Boundary::LowerUpper, Boundary::Underscore, Boundary::Hyphen, Boundary::Space, Boundary::Acronym]).to_case(Case::Kebab)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn dot_notation_to_css_compat() {
+		assert_eq!(
+			css_stringify(&"global.color.purple.100".to_string()),
+			String::from("global-color-purple-100")
+		);
+		
+		assert_eq!(
+			css_stringify(&"text.headings.h1.fontSize".to_string()),
+			String::from("text-headings-h1-font-size")
+		);
+	}
 }
