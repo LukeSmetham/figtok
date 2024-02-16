2b5c1033df25b834259ea648d3c40ca1c2e12179
Luke Smetham <luke@smetham.dev>
2024-02-16 11:23:20 +0100

    more tests cases for tokens/utils.rs

--- packages/tokens/src/utils.rs
+++ packages/tokens/src/utils.rs
@@ -1,5 +1,7 @@
 use convert_case::{Boundary, Case, Casing};
 
+/// Converts strings of various casings to a valid KebabCase CSS string that can be used for variable names
+/// class-names etc.
 pub fn css_stringify(s: &String) -> String {
 	let boundaries: [Boundary; 5] = [Boundary::LowerUpper, Boundary::Underscore, Boundary::Hyphen, Boundary::Space, Boundary::Acronym];
 	s.replace(".", "-").with_boundaries(&boundaries).to_case(Case::Kebab)
@@ -12,7 +14,9 @@ mod tests {
 
 	#[test_case("global.color.purple.100", "global-color-purple-100")]
 	#[test_case("text.headings.h1.fontSize", "text-headings-h1-font-size")]
-	fn dot_notation_to_css_compat(input: &str, expected: &str) {
+	#[test_case("myCompositionToken", "my-composition-token")]
+	#[test_case("ColorPalette.primaryColor.100", "color-palette-primary-color-100")]
+	fn to_css_compatible_string(input: &str, expected: &str) {
 		assert_eq!(css_stringify(&input.to_string()), expected.to_string());
 	}
 }
