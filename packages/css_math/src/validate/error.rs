b12012d6cc3c9ec223c3d5147eb60ee3e0485d9e
Luke Smetham <luke@smetham.dev>
2024-02-17 17:29:14 +0100

    cleanup old, no longer used, error enum cases

--- packages/css_math/src/validate/error.rs
+++ packages/css_math/src/validate/error.rs
@@ -3,14 +3,10 @@ pub enum ValidationError {
     DivisionByZero(String),
     IncompleteExpression(String),
     InvalidDivisionRHS(String),
-	InvalidToken(String),
-	InvalidWhitespace(String),
 	InvalidVariable(String),
 	InvalidNumber(String),
-	InvalidDivMulOperation(String),
     InvalidSyntax(String),
 	NoOperators(String),
 	MismatchedParentheses(String),
-    MixedUnitArithmetic(String),
     MultiplicationWithUnits(String)
 }
